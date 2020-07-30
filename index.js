87fc995557ad7eceed5f529f531fcbefa8d3030e
Sayantan Mishra <sayanmdn@gmail.com>
2020-07-30 10:35:08 +0530

    callback add

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const content = require('fs').readFileSync(__dirname + '/index.html', 'utf8');
 
 const httpServer = require('http').createServer((req, res) => {
-  // serve the index.html file
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Content-Length', Buffer.byteLength(content));
-  res.end(content);
+    // serve the index.html file
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Length', Buffer.byteLength(content));
+    res.end(content);
 });
 
 const io = require('socket.io')(httpServer);
 
 io.on('connect', socket => {
-  console.log('connect');
+    let counter = 0;
+    setInterval(() => {
+        socket.emit('hello', ++counter);
+        console.log(`No - ${counter}`)
+    }, 1000);
 });
 
 httpServer.listen(3000, () => {
-  console.log('go to http://localhost:3000');
+    console.log('go to http://localhost:3000');
 });
+
+
+// here i send an object (counter) to the browser, but i can also send a function in the place of an object with a callback
+// like in the example https://gist.github.com/daffl/0647ac1a85458389c3b253a1388eb194
